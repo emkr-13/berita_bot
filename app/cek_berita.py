567a998973eda6d5e5ff3d8158309e18fe202a60
emkr-13 <krisna.rata13@gmail.com>
2023-10-02 21:18:00 +0700

    first commit

---
+++ app/cek_berita.py
@@ -0,0 +1,79 @@
+import psycopg2
+from decouple import config
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def insert_news_data(title, link):
+    try:
+        # Konfigurasi koneksi ke database
+        db_host = config('POSTGRE_DB_HOST')
+        db_port = config('POSTGRE_DB_PORT')
+        db_name = config('POSTGRE_DB_NAME')
+        db_user = config('POSTGRE_DB_USER')
+        db_pass = config('POSTGRE_DB_PASS')
+
+        # Membuat koneksi ke database
+        conn = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+            user=db_user,
+            password=db_pass
+        )
+
+        # Membuat kursor untuk eksekusi perintah SQL
+        cursor = conn.cursor()
+
+        # Perintah SQL untuk memasukkan data ke dalam tabel "berita"
+        insert_query = "INSERT INTO berita (judul_berita, link) VALUES (%s, %s)"
+        cursor.execute(insert_query, (title, link))
+
+        # Commit perubahan ke database
+        conn.commit()
+
+        # Menutup kursor dan koneksi
+        cursor.close()
+        conn.close()
+
+        logger.info(f"Inserted news into database: {title}")
+
+    except Exception as e:
+        logger.error(f"Failed to insert news into database. Error: {str(e)}")
+
+def is_news_exists(title):
+    try:
+        # Konfigurasi koneksi ke database
+        db_host = config('POSTGRE_DB_HOST')
+        db_port = config('POSTGRE_DB_PORT')
+        db_name = config('POSTGRE_DB_NAME')
+        db_user = config('POSTGRE_DB_USER')
+        db_pass = config('POSTGRE_DB_PASS')
+
+        # Membuat koneksi ke database
+        conn = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+            user=db_user,
+            password=db_pass
+        )
+
+        # Membuat kursor untuk eksekusi perintah SQL
+        cursor = conn.cursor()
+
+        # Perintah SQL untuk memeriksa apakah berita dengan judul yang sama sudah ada dalam database
+        cursor.execute("SELECT COUNT(*) FROM berita WHERE judul_berita = %s", (title,))
+        count = cursor.fetchone()[0]
+
+        # Menutup kursor dan koneksi
+        cursor.close()
+        conn.close()
+
+        # Mengembalikan True jika berita sudah ada, False jika tidak
+        return count > 0
+
+    except Exception as e:
+        logger.error(f"Failed to check if news exists in the database. Error: {str(e)}")
+        return False
---
+++ app/main.py
@@ -0,0 +1,113 @@
+import feedparser
+import telegram
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from decouple import config
+from telegram import ChatAction
+import time
+import logging
+
+from cek_berita import insert_news_data,is_news_exists
+
+# Konfigurasi Logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+API_KEY = config('API_KEY')
+RSS_FEED_URLS = ['https://www.suara.com/rss/news', 'https://www.vice.com/id/rss?locale=id_id', 'https://www.cnnindonesia.com/nasional/rss', 'https://www.cnbcindonesia.com/news/rss', 'https://www.republika.co.id/rss', 'https://lapi.kumparan.com/v2.0/rss/']
+CHECK_INTERVAL = int(config('TIME_CHECKS'))
+
+bot = telegram.Bot(token=API_KEY)
+
+
+def fetch_latest_news():
+    all_news_entries = []
+    for url in RSS_FEED_URLS:
+        try:
+            feed = feedparser.parse(url)
+            # Pengecekan apakah feed memiliki judul dan tautan (bukan bot)
+            if 'title' in feed.feed and 'link' in feed.feed:
+                all_news_entries.extend(feed.entries)
+                logger.info(f"Fetched news from {url}")
+        except Exception as e:
+            logger.error(f"Failed to fetch news from {url}. Error: {str(e)}")
+    
+    all_news_entries.sort(key=lambda x: x.published_parsed, reverse=True)
+    return all_news_entries
+
+
+
+def send_news_updates(context: CallbackContext):
+    chat_id = context.job.context
+    news_entries = fetch_latest_news()
+    if news_entries:
+        for latest_news in news_entries:
+            title = latest_news.title
+            link = latest_news.link
+            # pub_date = latest_news.pubDate
+            message = f'<b>{title}</b>\n<a href="{link}">Read more</a>'
+            if not is_news_exists(title):
+                try:
+                    context.bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.HTML)
+                    logger.info(f"Sent news update to group chat {chat_id}: {title}")
+                    insert_news_data(title, link)  # Menambahkan tanggal publikasi ke dalam database
+                except Exception as e:
+                    if "Flood control exceeded" in str(e):
+                        logger.warning("Flood control exceeded. Retrying in 60 seconds...")
+                        time.sleep(60)  
+                        continue  
+                    else:
+                        logger.error(f"Failed to send news update to group chat {chat_id}: {title}. Error: {str(e)}")
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Welcome! Use the /news command to get the latest news.')
+
+
+def news(update: Update, context: CallbackContext):
+    news_entries = fetch_latest_news()
+    if news_entries:
+        latest_news = news_entries[0]
+        title = latest_news.title
+        link = latest_news.link
+        pub_date = latest_news.pubDate
+        message = f'<b>{title}</b>\n<a href="{link}">Read more</a>\n Tanggal: {pub_date}'
+        try:
+            update.message.reply_text(message, parse_mode=telegram.ParseMode.HTML)
+            logger.info("Sent news update to user")
+        except Exception as e:
+            logger.error(f"Failed to send news update to user. Error: {str(e)}")
+    else:
+        update.message.reply_text('No news available at the moment.')
+        logger.info("User requested news, but no news available")
+
+def error(update, context):
+    if update and update.message:
+        if 'Forbidden: bot can\'t send messages to bots' in str(context.error):
+            logger.warning(f'Bot is trying to send a message to another bot. Ignoring.')
+        else:
+            logger.warning(f'Update "{update}" caused error "{context.error}"')
+
+
+
+if __name__ == '__main__':
+    updater = Updater(API_KEY, use_context=True)
+    dp = updater.dispatcher
+
+    # Command handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('news', news))
+
+    # Error handler
+    dp.add_error_handler(error)
+
+    # Replace 'YOUR_GROUP_CHAT_ID' with the actual chat ID of your group
+    group_chat_id = int(config('GRUP_CHAT_ID'))
+
+    # Job queue for sending news updates automatically
+    job_queue = updater.job_queue
+    job_queue.run_repeating(send_news_updates, interval=CHECK_INTERVAL, context=group_chat_id)
+
+
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
